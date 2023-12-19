ed28c318d49395555abf6994b6def74850e36da7
Codex-Swapnil1 <swapnilparchakewrd2@gmail.com>
2023-12-19 23:45:04 +0530

    footer copyright added

--- iasureit_assignment/src/App.js
+++ iasureit_assignment/src/App.js
@@ -27,7 +27,13 @@ function App() {
       <div className='contact_us'>
 
       </div>
-      <iframe id="map-canvas" class="map_part" width="100%"  height="350"  frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%&amp;height=100%&amp;hl=en&amp;q=1B, 2nd, BUILDING-B3, CEREBRUM IT PARK, Marigold complex, Kalyani Nagar, Pune, Maharashtra 411014&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">Powered by <a href="https://embedgooglemaps.com">how to embed google maps generator</a> and <a href="https://yatzyregler.com/maxi-yatzy-regler/">Maxi Yatzy regler</a></iframe>
+      <iframe id="map-canvas" class="map_part" width="100%"  height="340"  frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%&amp;height=100%&amp;hl=en&amp;q=1B, 2nd, BUILDING-B3, CEREBRUM IT PARK, Marigold complex, Kalyani Nagar, Pune, Maharashtra 411014&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">Powered by <a href="https://embedgooglemaps.com">how to embed google maps generator</a> and <a href="https://yatzyregler.com/maxi-yatzy-regler/">Maxi Yatzy regler</a></iframe>
+      <footer>
+        <div className='copyright'>
+          <div>Copyright © 2023, iAssureIT All Rights Reserved </div>
+          <div> <span style={{color:"#e2eaf6"}}>Designed & Developed By</span> <span style={{fontWeight:"bold"}}>iAssure International Technologies Pvt. Ltd.</span> </div>
+        </div>
+      </footer>
     </div>
   );
 }
